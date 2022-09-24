5e35a84d956c71c7ea508bf98ef483c74cf7f7e6
alpbrace <ubeyde.alpkilic25@gmail.com>
2022-09-24 22:49:41 +0200

    update on calculator

--- JS/4-Ios-Calculator/app.js
+++ JS/4-Ios-Calculator/app.js
@@ -1,87 +1,77 @@
 const container = document.querySelector(".container");
 const result = document.querySelector(".result");
 
-
-
 let myArray = []
-let check = false;
+let equalClickedCheck = false;
+let operetorCheck = false;
 container.addEventListener("click", (e => {
- 
-  if (e.target.classList.contains("numbers")) {
-    document.querySelector(".result").innerText += e.target.innerText
 
-     if (result.innerHTML.length > 0){
-      document.querySelector(".box1").innerText="C"
-    } 
-    if (check) {
-      check = false;
+  if (e.target.classList.contains("numbers")) {
+
+    document.querySelector(".result").innerText += e.target.innerText
+    operetorCheck = true;
+    if (result.innerHTML.length > 0) {
+      document.querySelector(".box1").innerText = "C"
+    }
+    if (equalClickedCheck) {
+      equalClickedCheck = false;
       document.querySelector(".result").innerText = e.target.innerText
     }
-  } else if (e.target.classList.contains("operator")) {
-
-
+  } else if (e.target.classList.contains("operator") && operetorCheck) {
     document.querySelector(".result").innerText = document.querySelector(".result").innerText + e.target.innerText
-    check = false;
+    equalClickedCheck = false;
+    operetorCheck = false;
 
-  } else if (e.target.classList.contains("box19")) {
+  }  //? "=" sign  
+  else if (e.target.classList.contains("box19") && operetorCheck) {
     myArray.push(document.querySelector(".result").innerText);
- 
-    if (myArray[0].includes("%")) {
-      let regex = /[+*\%/-]/g;
-      const found = myArray[0].match(regex);
-      if (found.length === 1) {
-        result.innerText = (Number((myArray[0].slice(0, myArray[0].indexOf("%")))) / 100) * (Number(myArray[0].slice(myArray[0].indexOf("%") + 1)));
-      } else {
-        let stringArray = [...myArray[0]];
-        stringArray.map((item, index, array) => {
-          if (item === "%") {
-            item = item * 2;
+    let regex = /[x\%/÷]/g;
+    const found = myArray[0].match(regex);
+  
+    if (found !== null) {
+      if (found.length >= 1) {
+        let checkArray = [...myArray[0]];
+        checkArray.map((item, index, array) => {
+          if (item == "x") {
+            array[index] = "*"
+          } else if (item == "%") {
             array[index] = "*0.01*"
+          } else if (item == "÷") {
+            array[index] = "/"
           }
         })
-        let string = stringArray.join("");
-        
-        try{
-          let myresult = Function("return " + string)();
-          result.innerText = myresult
-          check=true;
-        }
-        catch(e){
-          check = false;
-        }
-      }
-
-    } else {
-      try{
+        myArray[0] = checkArray.join("");
+       
         let myresult = Function("return " + myArray[0])();
         result.innerText = myresult
-        check=true;
-      
+        equalClickedCheck = true;
       }
-      catch(e){
-        check = false;
-      }
-    
+    } else {
+      let myresult = Function("return " + myArray[0])();
+      result.innerText = myresult
+      equalClickedCheck = true;
+
     }
-    
+    myArray = []
+
+  }
+  //? "±" sign  
+  else if (e.target.classList.contains("box2")) {
+    if (result.innerHTML.includes("+") || result.innerHTML.includes("-")) {
+      let plusIndex = result.innerHTML.lastIndexOf("+");
+      let minusIndex = result.innerHTML.lastIndexOf("-");
+      let decideOperator = plusIndex > minusIndex ? "+" : "-";
+      let array = [...result.innerHTML]
+      let lastIndex = array.lastIndexOf(decideOperator);
+      array[lastIndex] = plusIndex > minusIndex ? "-" : "+";
+      result.innerHTML = array.join("");
+    }
+  } else if (e.target.classList.contains("box1")) {
+    result.innerHTML = "";
+    operetorCheck = true;
     myArray = [];
-   
-  }else if (e.target.classList.contains("box2")){
-    if(result.innerHTML.includes("+")|| result.innerHTML.includes("-")){
-      let plusIndex=result.innerHTML.lastIndexOf("+");
-      let minusIndex=result.innerHTML.lastIndexOf("-");
-      let decideOperator=plusIndex>minusIndex?"+":"-";
-      let array=[...result.innerHTML]
-      let lastIndex=array.lastIndexOf(decideOperator);
-      array[lastIndex]=plusIndex>minusIndex?"-":"+";
-      result.innerHTML=array.join("");
-    }
-  }else if (e.target.classList.contains("box1")){
-      let array=[...result.innerHTML]
-      array.pop()
-      result.innerHTML=array.join("");
-    if( result.innerHTML==0){
-      document.querySelector(".box1").innerText="AC"
+    if (result.innerHTML == 0) {
+      document.querySelector(".box1").innerText = "AC"
     }
   }
 }))
