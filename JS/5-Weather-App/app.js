a98870531ea97cc2e860d72a58ce476d5755fb66
alpbrace <ubeyde.alpkilic25@gmail.com>
2022-09-27 01:11:19 +0200

    weather app has been committed

---
+++ JS/5-Weather-App/app.js
@@ -0,0 +1,79 @@
+
+const weathers = document.querySelector(".weathers");
+const input = document.querySelector(".form-control")
+const submit = document.querySelector(".btn")
+let cityName;
+let cities = [];
+
+
+const fetchdata = (name) => {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=cfed455d0e51a873c9301547e0261e44&units=metric`)
+    .then((res) => {
+
+      if (!res.ok) {
+        if(name.length>0){
+          renderError();
+        }
+      
+      }
+      return res.json()
+    }).then((data) => renderData(data)).catch((error) => {})
+}
+
+const renderData = (data) => {
+
+  const { weather, main: { temp }, sys: { country } } = data
+
+  weathers.innerHTML += `
+     
+      <div class="card col-md-4 col-xxl-2 col-xl-3 col-sm-8 shadow-sm">
+      <i class="fa-sharp fa-solid fa-square-xmark"></i>
+        <h2 class="text-capitalize text-center mt-3">${cityName}<sup class="m-1 bg-info text-white">${country}</sup></h2>
+        <img src="http://openweathermap.org/img/wn/${weather[0].icon}@4x.png" class="card-img-top" alt="...">
+       
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item fw-semibold fs-2">${temp}<sup>°C</sup></li>
+          <li class="list-group-item fw-semibold fs-5 text-capitalize">${weather[0].description}</li>
+        </ul>
+      </div> `
+}
+
+const renderError = () => {
+
+    document.querySelector(".error").innerText = `${cityName} is not found`
+    document.querySelector(".error").classList.remove("invisible");
+  
+};
+
+
+submit.addEventListener("click", () => {
+  cityName = input.value;
+
+  if (!cities.includes(cityName)) {
+    fetchdata(cityName);
+    cities.push(cityName.toLowerCase());
+  } else if(input.value.length >0){
+    document.querySelector(".error").innerText = `${cityName} has been already added`
+    document.querySelector(".error").classList.remove("invisible");
+    input.value = ""
+    return;
+  }
+  document.querySelector(".error").classList.add("invisible");
+  input.value = ""
+
+
+})
+
+weathers.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-sharp")) {
+    e.target.parentElement.remove();
+    cities.splice((cities.indexOf(e.target.nextElementSibling.innerText.slice(0, e.target.nextElementSibling.innerText.length - 2))));
+
+  }
+})
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    submit.click();
+  }
+})
--- JS/README.md
+++ JS/README.md
@@ -5,14 +5,23 @@ Javascript Workshop contains hands-on trainings and projects.
 - [1-Checkout App](./1-Checkout_page/README.md)
 
 - Live Site URL: [Live site URL here](https://alpsabre.github.io/Front-End-Projects/JS/1-Checkout_page/)
+
 - [2-Diamond](./2-Diamond/README.md)
 
 - Live Site URL: [Live site URL here](https://alpsabre.github.io/Front-End-Projects/JS/2-Diamond/)
+
 - [3-Checkout Page with Event Capturing](./3-Checkout_page_with_event_capturing/README.md)
 
 - Live Site URL: [Live site URL here](https://alpsabre.github.io/Front-End-Projects/JS/3-Checkout_page_with_event_capturing/)
+
 - [4-Ios-Calculator](./4-Ios-Calculator/README.md)
+
 - Live Site URL: [Live site URL here](https://alpsabre.github.io/Front-End-Projects/JS/4-Ios-Calculator/)
+
+- [5-Weather-App](./5-Weather-App/README.md)
+
+- Live Site URL: [Live site URL here](https://alpsabre.github.io/Front-End-Projects/JS/4-Ios-Calculator/)
+
 
 
 
